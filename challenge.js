92fe1eb5b001ca731b0bbda8e76316ce3f1af83d
joyhuangg <joyhuangg@gmail.com>
2018-09-19 16:27:27 -0400

    Done.

---
+++ challenge.js
@@ -0,0 +1,90 @@
+let counter = document.getElementById('counter');
+let plus = document.getElementById('+')
+let minus = document.getElementById('-')
+let heart = document.getElementById('<3')
+let pause = document.getElementById('pause')
+let likes = document.querySelector('ul.likes');
+let commentList = document.querySelector('div#list')
+let submit = document.querySelector('button#submit')
+let usersComment = document.querySelector('form#comment-form input')
+let count = 0;
+let isPaused = false;
+
+function timing(){
+  if(!isPaused){
+    count++;
+    counter.innerText = count + "";
+  }
+}
+
+let timer = setInterval(timing,1000);
+
+
+const main = document.getElementById('main')
+
+plus.addEventListener('click', (e) => {
+  count++;
+  counter.innerText = count + "";
+  // console.log(count);
+})
+
+minus.addEventListener('click', (e) =>{
+  count--;
+  counter.innerText = count + "";
+})
+
+let likeCount;
+let current;
+heart.addEventListener('click', (e) => {
+
+  // increment likeCount, same second
+  if (current === count){
+    likeCount++;
+    let element = likes.children[likes.children.length-1];
+    result = `${count} has been liked ${likeCount} `
+    let time = likeCount > 1? "times":"time"
+    result += time;
+    element.innerHTML = result;
+  }
+  // create a new one, different second 
+  else {
+    current = count;
+    likeCount = 1;
+    let element = document.createElement('li');
+    result = `${count} has been liked ${likeCount} `
+    let time = likeCount > 1? "times":"time"
+    result += time;
+    element.innerHTML = result;
+    element.dataset.num = count;
+    likes.appendChild(element)
+  }
+  // likeCount++;
+  // result = `${count} has been liked ${likeCount} `
+  // let time = likeCount > 1? "times":"time"
+  // result += time;
+  // let element = document.createElement('li');
+  // element.innerHTML = result;
+  // element.dataset.num = count;
+  // likes.appendChild(element)
+  // debugger
+
+  //add li to list of likes
+  //need to use closure? so that counter restarts for each item?
+})
+
+pause.addEventListener('click', (e) => {
+  isPaused = !isPaused
+  pause.innerText = isPaused? "resume":"pause"
+
+})
+
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault()
+  usersComment = document.querySelector('form#comment-form input').value
+  document.body.querySelect('form#comment-form input').value = ""
+  let element = document.createElement('p');
+  element.innerHTML = usersComment;
+  commentList.appendChild(element)
+
+})
